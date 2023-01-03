e39295685f1040cd488dfdca7e9ff39354ef2218
Nikhil <nkarnwal07@gmail.com>
2023-01-03 23:19:42 +0530

    password setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 app.listen(5000);
