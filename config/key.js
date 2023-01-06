d189dc720f0ac79f8ae8a57a1bf276b48780c04b
Nikhil <nkarnwal07@gmail.com>
2023-01-06 21:42:47 +0530

    oauth callback completed

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "571335818463-vmapvgf1m2fvqeopr5s7lio824vc63h9.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
+const express = require("express");
 const app = express();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/key");
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => console.log(accessToken)
+  )
+);
+
+app.get(
+  "/auth/google/",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(5000);
