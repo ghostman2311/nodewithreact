8f9bfb418fba865c0ae0b050b2d28aa03f084f05
Nikhil <nkarnwal07@gmail.com>
2023-01-09 23:02:40 +0530

    server structure refactor

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 const express = require("express");
+require("./services/passport");
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/key");
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken) => console.log(accessToken)
-  )
-);
-
-app.get(
-  "/auth/google/",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 app.listen(5000);
